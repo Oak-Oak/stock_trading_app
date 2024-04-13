8cd1b90e288abd0a60934ba45e46d3cb54a460af
GARS <christian.garcia4297@gmail.com>
2024-04-13 11:53:31 +0800

    Feat: Implement role based redirection after sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    protected
+
+    def after_sign_in_path_for(resource)
+        if resource.role == 'admin'
+          admins_dashboard_path
+        else
+          traders_dashboard_path
+        end
+      end
 end
