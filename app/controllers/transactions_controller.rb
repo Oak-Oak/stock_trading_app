d64a786cf98cc1aed2caf529f45c4527dbaeac6f
GARS <christian.garcia4297@gmail.com>
2024-04-13 10:51:18 +0800

    feat: update transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,10 +1,27 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @transactions = current_user.transactions
   end
 
   def new
+    @transaction = current_user.transactions.build
   end
 
   def create
+    @transaction = current_user.transactions.build(transaction_params)
+    if @transaction.save
+      redirect_to transactions_path, notice: "Transaction successful."
+    else
+      render :new
+    end
   end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:action, :symbol, :quantity, :price)
+  end
+  
 end
