43c491bc0eb2af40ac2b4afd892e029e55127575
GARS <christian.garcia4297@gmail.com>
2024-04-13 14:37:22 +0800

    Fix: modified dashboard controller

--- app/controllers/admins/dashboard_controller.rb
+++ app/controllers/admins/dashboard_controller.rb
@@ -1,5 +1,19 @@
 class Admins::DashboardController < ApplicationController
     def index
-        @traders = User.where(role: 'trader')
-    end
+        @traders = User.where(isAdmin: false)
+        @pending_traders = User.where(isAdmin: false, approved: false)
+      end
+
+      def approve_trader
+        trader = User.find(params[:id])
+        trader.update(approved: true)
+        redirect_to admins_dashboard_path, notice: "#{trader.email} has been approved as a trader."
+      end
+    
+      def reject_trader
+        trader = User.find(params[:id])
+        trader.destroy
+        redirect_to admins_dashboard_path, notice: "#{trader.email} has been rejected and removed."
+      end
+
 end
