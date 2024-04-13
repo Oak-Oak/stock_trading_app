b647d2b2d682b5570ed2744be207cb3d410b993b
GARS <christian.garcia4297@gmail.com>
2024-04-13 10:47:51 +0800

    Add: Transactions Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'transactions/index'
+  get 'transactions/new'
+  get 'transactions/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
