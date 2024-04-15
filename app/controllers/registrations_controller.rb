cea5dc3e557da5270def1a6dd9f48cd625578862
GARS <christian.garcia4297@gmail.com>
2024-04-15 16:42:19 +0800

    Feat: modified controller to implement mailers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,26 +1,38 @@
 class RegistrationsController < Devise::RegistrationsController
-    def create
-      if params[:user][:isAdmin] == 'true'
-        params[:user][:approved] = true
-      else
-        params[:user][:approved] = false
-      end
-  
-      super
+  def create
+    if params[:user][:isAdmin] == 'true'
+      params[:user][:approved] = true
+    else
+      params[:user][:approved] = false
     end
-  
-    def new
-        @user = User.new
-    
-        if current_user && current_user.isAdmin
-          redirect_to admins_dashboard_path
-        elsif current_user && !current_user.approved
-          render 'traders/pending_approval'
-        else
-          render 'devise/registrations/new'
-        end
-      end
-      
-      
+
+    @user = User.new(user_params)
+
+    if @user.save
+      UserMailer.account_confirmation(@user).deliver_now
+      UserMailer.account_pending_admin(@user).deliver_now
+      redirect_to traders_dashboard_path
+    else
+      render :new
+    end
   end
+
+  def new
+    @user = User.new
+
+    if current_user && current_user.isAdmin
+      redirect_to admins_dashboard_path
+    elsif current_user && !current_user.approved
+      render 'traders/pending_approval'
+    else
+      render 'devise/registrations/new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :approved, :isAdmin)
+  end  
+end
   
