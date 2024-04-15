d44c992dce2fa3033436798dc056ab2ccd6300cb
GARS <christian.garcia4297@gmail.com>
2024-04-15 13:35:53 +0800

    Fix: transferred create trader action to separate controller

--- app/controllers/admins/dashboard_controller.rb
+++ app/controllers/admins/dashboard_controller.rb
@@ -16,19 +16,5 @@ class Admins::DashboardController < ApplicationController
         redirect_to admins_dashboard_path, notice: "#{trader.email} has been rejected and removed."
       end
 
-      def create_trader
-        @user = User.new(user_params)
-        if @user.save
-          redirect_to admins_dashboard_path, notice: "Trader created successfully."
-        else
-          flash[:alert] = @user.errors.full_messages.join(", ")
-          redirect_to admins_dashboard_path
-        end
-      end
-    
-      private
-    
-      def user_params
-        params.require(:user).permit(:email, :password, :approved, :isAdmin)
-      end
+
 end
