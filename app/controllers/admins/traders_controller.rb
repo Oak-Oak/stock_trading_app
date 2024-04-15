bb2919698cfe74ab1d79e0da4c5bc014abc325c4
GARS <christian.garcia4297@gmail.com>
2024-04-15 13:35:23 +0800

    Add: Traders controller for handling show create edit and delete of new traders

---
+++ app/controllers/admins/traders_controller.rb
@@ -0,0 +1,37 @@
+class Admins::TradersController < ApplicationController
+    before_action :set_trader, only: [:show, :edit, :update]
+
+    def show
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @trader.update(user_params)
+        redirect_to admins_trader_path(@trader), notice: "Trader updated successfully."
+      else
+        render :edit
+      end
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          redirect_to admins_dashboard_path, notice: "Trader created successfully."
+        else
+          flash[:alert] = @user.errors.full_messages.join(", ")
+          redirect_to admins_dashboard_path
+        end
+      end
+  
+    private
+  
+    def set_trader
+      @trader = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :password, :approved, :isAdmin)
+    end
+end
