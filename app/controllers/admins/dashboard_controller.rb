f7a910511416102b2cd53ec8e28d7e9cdd0bc4eb
GARS <christian.garcia4297@gmail.com>
2024-04-18 20:04:14 +0800

    Fix: added before action for authenticating admin access

--- app/controllers/admins/dashboard_controller.rb
+++ app/controllers/admins/dashboard_controller.rb
@@ -1,22 +1,32 @@
 class Admins::DashboardController < ApplicationController
-    def index
-        @traders = User.where(isAdmin: false, approved: true).order(created_at: :asc)
-        @pending_traders = User.where(isAdmin: false, approved: false).order(created_at: :asc)
-      end
+  before_action :authenticate_admin
 
-      def approve_trader
-        trader = User.find(params[:id])
-        UserMailer.account_approved(trader).deliver_now
-        trader.update(approved: true)
-        redirect_to admins_dashboard_path, notice: "#{trader.email} has been approved as a trader."
-      end
+  def index
+    @traders = User.where(isAdmin: false, approved: true).order(created_at: :asc)
+    @pending_traders = User.where(isAdmin: false, approved: false).order(created_at: :asc)
+  end
+
+  def approve_trader
+    trader = User.find(params[:id])
+    UserMailer.account_approved(trader).deliver_now
+    trader.update(approved: true)
+    redirect_to admins_dashboard_path, notice: "#{trader.email} has been approved as a trader."
+  end
     
-      def reject_trader
-        trader = User.find(params[:id])
-        UserMailer.account_rejected(trader).deliver_now
-        trader.destroy
-        redirect_to admins_dashboard_path, notice: "#{trader.email} has been rejected and removed."
-      end
+  def reject_trader
+    trader = User.find(params[:id])
+    UserMailer.account_rejected(trader).deliver_now
+    trader.destroy
+    redirect_to admins_dashboard_path, notice: "#{trader.email} has been rejected and removed."
+  end
 
+  private
+
+  def authenticate_admin
+    unless current_user && current_user.isAdmin
+      flash[:alert] = "You are not authorized to access this page."
+      redirect_to root_path
+    end
+  end
 
 end
