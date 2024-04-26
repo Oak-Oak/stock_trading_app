4a3750d80be75a6cea3afcf337ff07aaeb4821a4
GARS <christian.garcia4297@gmail.com>
2024-04-26 17:01:55 +0800

    Fix: updated expired tokens

--- config/initializers/iex_api.rb
+++ config/initializers/iex_api.rb
@@ -1,5 +1,5 @@
 IEX::Api.configure do |config|
-  config.publishable_token = 'pk_ec18920bd35e4964b9f495d511ea60c1' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
-  config.secret_token = 'sk_1c6ac79f63034f97bb749735d0abfa8a' # defaults to ENV['IEX_API_SECRET_TOKEN']
+  config.publishable_token = 'pk_0553f5c0b81949e9b83f478b23aba19f' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+  config.secret_token = 'sk_7b7a869cf03045a08e218763e413ddd0' # defaults to ENV['IEX_API_SECRET_TOKEN']
   config.endpoint = 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
 end
