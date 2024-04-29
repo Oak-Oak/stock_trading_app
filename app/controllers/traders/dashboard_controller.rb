ab8d1bb2c7f8559668c9c6a51827dbcb397dc697
ianrebulado <eilrebulado@gmail.com>
2024-04-30 02:11:22 +0800

    chore: temp styles

--- app/controllers/traders/dashboard_controller.rb
+++ app/controllers/traders/dashboard_controller.rb
@@ -1,12 +1,17 @@
 class Traders::DashboardController < ApplicationController
   before_action :authenticate_trader
+  before_action :initialize_iex
 
     def index
       @transactions = current_user.transactions.group_by(&:symbol)
       @total_quantity_per_stock = calculate_total_quantity_per_stock(@transactions)
+
+      fetch_stock_quote if params[:symbol].present?
     end
   
     private
+
+   
 
     def authenticate_trader
       if current_user.nil? 
@@ -27,5 +32,16 @@ class Traders::DashboardController < ApplicationController
   
       total_quantity_per_stock
     end
+
+    def initialize_iex
+      @client = IEX::Api::Client.new
+    end
+    
+    def fetch_stock_quote
+      @client = IEX::Api::Client.new
+      @symbol = params[:symbol]
+      @stock_quote = @client.quote(@symbol)
+    end
   end
   
+
