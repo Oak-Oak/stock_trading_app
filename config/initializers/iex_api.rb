20dbd36884be957940d7ebfb2b2af49874e69c44
ianrebulado <eilrebulado@gmail.com>
2024-05-06 08:52:30 +0800

    chore: add new tokens

--- config/initializers/iex_api.rb
+++ config/initializers/iex_api.rb
@@ -1,5 +1,5 @@
 IEX::Api.configure do |config|
-  config.publishable_token = 'pk_0553f5c0b81949e9b83f478b23aba19f' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
-  config.secret_token = 'sk_7b7a869cf03045a08e218763e413ddd0' # defaults to ENV['IEX_API_SECRET_TOKEN']
+  config.publishable_token = 'pk_7ebdbe7189304add85d5f3d8a033a1db' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+  config.secret_token = 'sk_5c11cc66d27048de86b719f10219635e' # defaults to ENV['IEX_API_SECRET_TOKEN']
   config.endpoint = 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
 end
